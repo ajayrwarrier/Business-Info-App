d54d8dcc949f44960157c8928137970e2f599e1c
Ajay R Warrier <ajayrwarrier@gmail.com>
2016-10-18 10:48:53 +0530

    Working App
    Complete Info about local school business

---
+++ app/src/main/java/com/example/ajayrwarrier/busiinfo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ajayrwarrier.busiinfo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView callbutton = (ImageView) findViewById(R.id.imageButton);
+        callbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "04872366660"));
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+
+                    return;
+                }
+                startActivity(intent);
+            }
+        });
+    }
+}
